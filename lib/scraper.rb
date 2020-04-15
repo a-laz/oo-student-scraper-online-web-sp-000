a948ca74206bfb6b2511e06340cfc81f1896df10
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-04-15 15:56:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,11 +6,12 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     #Student URL
-    doc.css(".student-card a").map { |link| link['href'] }
+    #doc.css(".student-card a").map { |link| link['href'] }
     #Student Name
-    doc.css(".student-card .card-text-container h4").map {|name| name.text}
+    #doc.css(".student-card .card-text-container h4").map {|name| name.text}
     #Student Location
-    doc.css(".student-card .card-text-container p").map {|location| location.text}
+    #doc.css(".student-card .card-text-container p").map {|location| location.text}
+    students = {}
     binding.pry
   end
 
