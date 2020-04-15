2fbc1d41056369e4625ce4ad2fdc9dcf7520bf34
Alex Lazarev <alex.m.lazarev@gmail.com>
2020-04-15 15:39:10 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ class Scraper
 
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
-    doc.css(".student-card").css("href").text
+    doc.css(".student-card").css("href")
     binding.pry
   end
 
